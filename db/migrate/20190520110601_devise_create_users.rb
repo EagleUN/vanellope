c5bc5b9ae6ad8fe4e495094c4740345db20a4181
Christian <cdsanabriar@unal.edu.co>
2019-05-20 15:15:33 -0500

    make the seeds and two tests

--- db/migrate/20190520110601_devise_create_users.rb
+++ db/migrate/20190520110601_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users, id: :uuid do |t|
       ## Database authenticatable
-      t.string :name
-      t.string :last_name
+      t.string :name,               null: false, default: ""
+      t.string :last_name,          null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -38,7 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    #add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(
+	name: "Christian",
+	last_name: "Sanabria",
+	email: "cdsanabriar@unal.edu.co",
+	password: '12345678',
+	password_confirmation: '12345678'
+	)
+user.save!
+
+user = User.new(
+	name: "Juan Sebastian",
+	last_name: "Chaves",
+	email: "jschavesr@unal.edu.co",
+	password: '12345678',
+	password_confirmation: '12345678'
+	)
+user.save!
+
+user = User.new(
+	name: "Laura",
+	last_name: "Santos",
+	email: "lbsantosg@unal.edu.co",
+	password: '12345678',
+	password_confirmation: '12345678'
+	)
+user.save!
+
+user = User.new(
+	name: "Diego",
+	last_name: "Niquefa",
+	email: "dsniquefav@unal.edu.co",
+	password: '12345678',
+	password_confirmation: '12345678'
+	)
+user.save!
+
+user = User.new(
+	name: "Juan Diego",
+	last_name: "Moreno",
+	email: "judmorenomo@unal.edu.co",
+	password: '12345678',
+	password_confirmation: '12345678'
+	)
+user.save!
+
