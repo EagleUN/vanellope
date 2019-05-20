7e66ae20bab8360ef2bac58584b0bc1219e6c227
Christian <cdsanabriar@unal.edu.co>
2019-05-20 06:16:40 -0500

    config the email regex, add name and lastname field and fix bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_095020) do
+ActiveRecord::Schema.define(version: 2019_05_20_110601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -226,13 +226,13 @@ ActiveRecord::Schema.define(version: 2019_05_20_095020) do
 #   Unknown type 'geometry' for column 'the_geom'
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
