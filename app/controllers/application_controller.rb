8bd10c808ea81ee26b2ab4725b9e8dc31ad661c0
Christian <cdsanabriar@unal.edu.co>
2019-06-22 14:56:37 -0500

    radical modification for the implementation of tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
-class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
+class ApplicationController < ActionController::API
+	
+	# Include Knock within your application.
+	include Knock::Authenticable
 
 	protected
 
-	def configure_permitted_parameters
-    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name])
-    	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name])
- 	end
-
- 	protect_from_forgery with: :null_session
-
+	# Method for checking if current_user is admin or not.
+	def authorize_as_admin
+		return_unauthorized unless !current_user.nil? && current_user.is_admin?
+	end
 end
